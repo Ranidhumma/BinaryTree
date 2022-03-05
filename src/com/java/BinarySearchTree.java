54870eaf35aca6562fa2c296ce3340ffa67ef3d5
Ranidhumma <ranidhumma8@gmail.com>
2022-03-05 23:29:31 +0530

    adding node tree and Printing order way

--- src/com/java/BinarySearchTree.java
+++ src/com/java/BinarySearchTree.java
@@ -10,7 +10,67 @@ public class BinarySearchTree {
 
 	public static void main(String[] args) {
 		scanner = new Scanner(System.in);
-
+		Node root = CreateTree();
 		System.out.println("welcome");
+		inOrder(root);
+		System.out.println();
+		preOrder(root);
+		System.out.println();
+		postOrder(root);
+		System.out.println();
+
+	}
+
+	/* created CreateTree Method , consider initial root value is null */
+
+	public static Node CreateTree() {
+		Node root = null;
+
+		System.out.println("Enter data: ");
+		int data = scanner.nextInt(); // got data
+
+		if (data == -1) {
+			// System.out.println("null");
+			return null;
+		}
+		root = new Node(data); // created node
+
+		System.out.println("enter left for " + data);
+		root.left = CreateTree();
+
+		System.out.println("enter right for " + data);
+		root.right = CreateTree();
+
+		return root;
+
+	}
+
+	/* inOrder */
+	static void inOrder(Node root) {
+		if (root == null)
+			return;
+
+		inOrder(root.left);
+		System.out.print(root.data + " ");
+		inOrder(root.right);
+	}
+
+	/* preOrder */
+	public static void preOrder(Node root) {
+		if (root == null)
+			return;
+		System.out.print(root.data + " ");
+		preOrder(root.left);
+		preOrder(root.right);
+	}
+
+	/* postOrder */
+	public static void postOrder(Node root) {
+		if (root == null)
+			return;
+
+		postOrder(root.left);
+		postOrder(root.right);
+		System.out.print(root.data + " ");
 	}
 }
